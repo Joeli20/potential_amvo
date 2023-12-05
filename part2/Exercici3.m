26dd5fca9b70e37fb955f589eb98a8c5be2cdc98
jordig22 <jordi.gallart.martinez@estudiantat.upc.edu>
2023-12-05 12:12:34 +0100

    Inicialització ala

--- part2/Exercici3.m
+++ part2/Exercici3.m
@@ -1,28 +1,27 @@
-clc;
-clear;
-close all;
-
-%% Input values
-% Wing Geometry
-b = 4.5; %[m] wingspan
-c_r = 0.9; %[m] chord at the root
-c_t = 0.6; %[m] chord at the tip
-AoA = 0; %[º] Angle of attack
-
-% Other Wing values
-m = 40; %[kg] mass of the wing
-rho = 1.225; %[kg/m^3] air density
-g = 9.81; %[m/s^2] acceleration of gravity
-
-Cl_0 = 0.24;
-Cl_a = 6.7;
-
-%% Previous calculations
-S = b * ((c_r + c_t)/2);
-W = m*g;
-
-Cl = Cl_0 + Cl_a*AoA;
-
-%% Main matter
-
-U_inf = sqrt((2*W)/(rho*S*CL));
+N = 9;
+x = linspace(-2.25,2.25,N);
+x_c = zeros(N-1,1);
+c = zeros(N-1,1);
+theta = zeros(N-1,1);
+c_t = 0.6;
+c_r = 0.9;
+theta_r = 0;
+theta_t = 0.3;
+m_chord_right = (c_t-c_r)/x(N);
+n_chord_right = c_r;
+m_theta_right = (theta_t-theta_r)/x(N);
+n_theta_right = theta_r;
+m_chord_left = (c_t-c_r)/x(1);
+n_chord_left = c_r;
+m_theta_left = (theta_t-theta_r)/x(1);
+n_theta_left = theta_r;
+for i = 1:N-1
+    x_c(i) = (x(i)+x(i+1))/2;
+    if i<N/2
+    c(i) = m_chord_left*x_c(i)+n_chord_left;
+    theta(i) = m_theta_left*x_c(i)+n_chord_left;
+    else
+    c(i) = m_chord_right*x_c(i)+n_chord_right;
+    theta(i) = m_theta_right*x_c(i)+n_chord_right;
+    end
+end
